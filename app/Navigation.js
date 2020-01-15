f7cada1e173d3ad58470a415f15f5d20f9b68109
apabook <apabook@MacBook-Pro-de-apabook.local>
2020-01-15 12:41:41 +0100

    Commit Style and navigation changes

---
+++ app/Navigation.js
@@ -0,0 +1,164 @@
+
+/**
+ * Programmatic Navigation and Page Transitions
+ * 
+ * 
+ */
+
+
+/**
+* Global vars
+*
+*/
+let links = GAME_UI.app.querySelectorAll('a[href^="#"]');
+let sections = GAME_UI.app.querySelectorAll('section');
+let modals = GAME_UI.app.querySelector('modal_window');
+
+
+
+/**
+ * 
+ */
+const getLinkIdParent = (link) => {
+    let id = '';
+    let currElement = link.parentElement;
+    while (currElement.id == '') {
+        currElement = currElement.parentElement;
+        id = currElement.id;
+    }
+    return '#' + id;
+};
+
+
+/**
+ * @function navigateTo
+ * Init navigation
+ * 
+ */
+const initNavigation = () => {
+    links.forEach(link => {
+        link.addEventListener('click', ev => {
+            ev.preventDefault();
+            navigateTo(link.hash, getLinkIdParent(link));
+        });
+    });
+};
+
+
+/**
+ * @function navigateTo
+ * Como la navegación está basada en bloques que se ven y no.
+ * Simplemente esconde todo y muestra el elemento a navegar
+ * 
+ * @param getTo: @default = #menu_page -> Es el ID a navegar. Por defecto es #menu_page
+ * 
+*/
+const navigateTo = (getTo = '#menu_page', getFrom = '') => {
+    let isModal =
+        document.querySelector(getTo).classList.contains('modal_window') ||
+        document.querySelector(getTo).classList.contains('spinner_window');
+
+    if (isModal) {
+        document.querySelector(getTo).classList.add('active');
+    } else {
+
+        // // distribute navigations....
+
+        // sections.forEach(section => section.classList.remove('active'));
+        // document.querySelector(getTo).classList.add('active');
+        // location.hash = getTo;
+
+        console.log(getTo, getFrom);
+        if (getTo == "#settings_page" && getFrom == "#menu_page") {
+            animation_slideOut(getTo, getFrom);
+        }
+    }
+};
+
+
+
+const animation_slideOut = (getTo, getFrom) => {
+    let going = document.querySelector(getFrom);
+    let coming = document.querySelector(getTo);
+    anime({
+        targets: going,
+        translateX: '-100%',
+        easing: 'easeOutQuad',
+        duration: 200
+    });
+    anime({
+        targets: coming,
+        translateX: 0,
+        easing: 'easeOutQuad',
+        opacity: 1,
+        duration: 200
+
+    });
+};
+
+
+/**
+ * @function getBack
+ * @alias navigateTo() 
+ * 
+ * Just for debuggin purposes
+ * 
+ */
+const getBack = () => {
+    navigateTo();
+};
+
+
+/**
+ * @function show/hideSpinner
+ * @alias navigateTo("#spinner_window") 
+ * 
+ */
+const showSpinner = () => {
+    GAME_UI.state.spinning = true;
+    GAME_UI.modalWindows.spinner.classList.add('active');
+};
+const hideSpinner = () => {
+    GAME_UI.state.spinning = false;
+    GAME_UI.modalWindows.spinner.classList.remove('active');
+};
+
+
+/**
+ * @function init
+ * Poner la navegación a funcionar. IIFE
+ * 
+*/
+const init = (() => {
+    initNavigation();
+
+    // Fake splash
+    // setTimeout(() => {
+    //     navigateTo('#menu_page');
+    // }, 1600);
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * No sabía donde ponerlo....
+ * 
+*/
+let toggles = document.querySelectorAll('.toggle');
+toggles.forEach(t => {
+    t.addEventListener('click', () => {
+        t.classList.toggle('on');
+    });
+});
+
+navigateTo('#splash_page');
---
+++ app/main.js
@@ -0,0 +1,62 @@
+const OPPONENT_HEIGHT = 5,
+    OPPONENT_PICTURE = "assets/img/malo.png",
+    OPPONENT_PICTURE_DEAD = "assets/img/malo_muerto.png",
+    OPPONENT_SPEED = 5,
+    OPPONENT_WIDTH = 5,
+    GAME_OVER_PICTURE = "assets/img/game_over.png",
+    KEY_LEFT = "LEFT",
+    KEY_RIGHT = "RIGHT",
+    KEY_SHOOT = "SHOOT",
+    MIN_TOUCHMOVE = 20,
+    PLAYER_HEIGHT = 5,
+    PLAYER_PICTURE = "assets/img/bueno.png",
+    PLAYER_PICTURE_DEAD = "assets/img/bueno_muerto.png",
+    PLAYER_SPEED = 20,
+    PLAYER_WIDTH = 5,
+    SHOT_HEIGHT = 1.5,
+    SHOT_SPEED = 20,
+    SHOT_PICTURE_PLAYER = "assets/img/shot1.png",
+    SHOT_PICTURE_OPPONENT = "assets/img/shot2.png",
+    SHOT_WIDTH = 1.5,
+
+    GAME_UI = {
+        app: document.querySelector('#app'),
+        pages: {
+            splash: document.querySelector('#splash_page'),
+            menu: document.querySelector('#menu_page'),
+            main: document.querySelector('#main_page'),
+            settings: document.querySelector('#settings_page'),
+            leaderboard: document.querySelector('#leaderboard_page'),
+            themes: document.querySelector('#themes_page'),
+        },
+        modalWindows: {
+            pause: document.querySelector('#modal_pause_window'),
+            spinner: document.querySelector('#modal_loading_spinner')
+        },
+        state: {
+            navigationStage: 'splash',
+            playing: false,
+            paused: false,
+            spinning: false
+        }
+    };
+
+function getRandomNumber(range) {
+    return Math.floor(Math.random() * range);
+}
+
+function collision(div1, div2) {
+    const a = div1.getBoundingClientRect(),
+        b = div2.getBoundingClientRect();
+    return !(a.bottom < b.top || a.top > b.bottom || a.right < b.left || a.left > b.right);
+
+}
+
+var game;
+function startGame() {
+    game = new Game();
+    game.start();
+    document.getElementById("pause").addEventListener("click", (e) => {
+        game.pauseOrResume();
+    });
+}
